a22a054ed7770e6f0943e60704c06a5a96e052da
bryant <kleinb1616@gmail.com>
2020-12-31 10:04:26 -0500

    need to make a favorite branch

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -5,9 +5,16 @@ import {getUser} from '../../store/user';
 
 function Profile() {
     const {userId} = useParams();
-    const userStateInfor = useSelector((state)=>{
+    const trainerState = useSelector((state)=>{
+      return state.trainer;
+    })
+    const userStateInfo = useSelector((state)=>{
       return state.user
     });
+    // let trainersToFindIds = userStateInfo.map(fav => fav.trainerId );
+    // let trainers = trainerState.map(train=> trainersToFindIds.include(train.id))
+    // console.log(trainersToFindIds)
+    // console.log(trainers)
 
 
     return (
