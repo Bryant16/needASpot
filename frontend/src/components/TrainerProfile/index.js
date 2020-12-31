538c93f7248cab2827c31d3a18d428ea835578a4
bryant <kleinb1616@gmail.com>
2020-12-31 09:05:37 -0500

    css for fav button

--- frontend/src/components/TrainerProfile/index.js
+++ frontend/src/components/TrainerProfile/index.js
@@ -1,51 +1,55 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {newFavoriteTrainer, removeFavoriteTrainer} from '../../store/user';
 import {getUserFavorites} from '../../store/user';
 import {getAllTrainers} from '../../store/trainer';
+import './TrainerProfile.css';
 
 function TrainerProfile() {
   const dispatch = useDispatch();
+  const { id } = useParams();
+
   const trainers = useSelector((state) => {
     return state.trainer;
   });
   const favorites = useSelector((state)=>{
     return state.user;
   });
-
-  const sessionUser = useSelector(state => state.session.user)
-  const { id } = useParams();
   const trainerLookUpId = Number(id);
+  let trainerIsFavorited = favorites.filter((fav)=> Number(fav.trainerId) === trainerLookUpId)
+  // console.log(trainerIsFavorited)
+  const [activeButton, setActiveButton] = useState(!trainerIsFavorited.toString());
+  // console.log(activeButton)
+  const sessionUser = useSelector(state => state.session.user)
   const userId = Number(sessionUser.id);
   console.log(userId, trainerLookUpId )
   const trainerDetails = trainers.filter((train) => train.id === trainerLookUpId);
   const singleTrainer = trainerDetails[0];
-  const favoritesForCurrentUser = singleTrainer.Favorites;
-  // const userLikedThisTrainer = favoritesForCurrentUser.filter(fav => fav.userId === sessionUser.id)
   
-  // console.log(userLikedThisTrainer)
-  console.log(favorites)
-  let trainerIsFavorited = favorites.filter((fav)=> Number(fav.trainerId) === trainerLookUpId)
-    // console.log(trainerIsFavorited)
+  
+  // console.log(favorites)
   const favorite = (e)=>{
     e.preventDefault();
     if(trainerIsFavorited.length > 0){
       console.log('unlike')
+      setActiveButton(true)
       dispatch(removeFavoriteTrainer(userId,trainerLookUpId))
     }else{
       console.log('like')
+      setActiveButton(false)
       dispatch(newFavoriteTrainer(userId,trainerLookUpId))
     }
-    // dispatch(getAllTrainers())
   }
   useEffect(()=>{
-    dispatch(getUserFavorites(sessionUser.id))
+    dispatch(getUserFavorites(sessionUser.id));
   },[dispatch])
-  // singleTrainer.Reviews.forEach((review) => console.log(review.review));
+
   return (
     <div>
-      <h1>Name: {trainerDetails[0].name}<button onClick={favorite}>Favorite</button></h1>
+      <h1>Name: {trainerDetails[0].name}
+      <button className={activeButton ? `favoriteButton` : null} onClick={favorite}>Favorite</button>
+      </h1>
       <h2>{singleTrainer.certifications}</h2>
       <h2>reviews</h2>
       {singleTrainer.Reviews.map((review) => (
